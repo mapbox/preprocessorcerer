c2cd4bf97f1f566a9821f50d22fcfc6396d14143
Ryan Clark <ryan.clark.j@gmail.com>
2015-02-04 15:41:14 -0700

    sketch shapeindex support

---
+++ preprocessors/shp-index.preprocessor.js
@@ -0,0 +1,49 @@
+var path = require('path'),
+    fs = require('fs'),
+    spawn = require('child_process').spawn,
+    shapeindex = path.resolve(__dirname, '..', 'node_modules', '.bin', 'mapnik-shapeindex.js');
+
+module.exports = function(infile, outfile, callback) {
+  spawn(shapeindex, ['--shape_files', infile])
+    .once('error', function(err) {
+      callback(err);
+    })
+    .on('exit', function() {
+      callback();
+    });
+};
+
+module.exports.description = 'Add a spatial index to shapefile';
+
+module.exports.criteria = function(infile, info, callback) {
+  if (info.filetype !== 'shp') return callback(null, false);
+
+  var ext = path.extname(infile),
+      dir = ext === '.shp' ? path.dirname(infile) : infile,
+      name = ext === '.shp' ? path.basename(infile, '.shp') : '';
+
+  fs.readdir(dir, function(err, files) {
+    if (err) return callback(err);
+
+    var shp, re;
+
+    if (!name) {
+      shp = files.filter(function(filename) {
+        return path.extname(filename) === '.shp';
+      });
+
+      if (!shp.length) return callback(new Error('Could not locate shapefile'));
+      name = path.basename(shp[0], '.shp');
+    }
+
+    re = new RegExp('^' + name);
+
+    files = files.filter(function(filename) {
+      var ext = path.extname(filename);
+      return re.test(filename) && ext === '.index';
+    });
+
+    if (files.length) return callback(null, false);
+    callback(null, true);
+  });
+};
