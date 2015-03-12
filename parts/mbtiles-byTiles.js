d2afcf54b759680836c31d31712c68ad4a3f8f71
Jake Pruitt <jrpruit1@asu.edu>
2015-03-12 19:49:12 -0400

    Added logic for mbtiles without tiles tables

--- parts/mbtiles-byTiles.js
+++ parts/mbtiles-byTiles.js
@@ -4,12 +4,26 @@ module.exports = function splitBySize(filepath, info, callback) {
   new Mbtiles(filepath, function(err, src) {
     if (err) return callback(err);
 
-    src._db.get('SELECT COUNT(1) AS count FROM tiles', function(err, result) {
-      if (err) return callback(err);
+    src._db.get('SELECT COUNT(1) AS count FROM tiles', function(err, tilesResult) {
+      var re = new RegExp('no such table: tiles');
+      if (err && re.test(err.message)) {
+        tilesResult = { count: 0 };
+      }
+      else if (err) return callback(err);
 
-      var maxTilesPerJob = 100000;
+      src._db.get('SELECT COUNT(1) AS count FROM grids', function(err, gridsResult) {
+        var re = new RegExp('no such table: grids');
+        if (err && re.test(err.message)) {
+          gridsResult = { count: 0 };
+        }
+        else if (err) return callback(err);
 
-      callback(null, Math.min(50, Math.ceil(result.count / maxTilesPerJob)));
+        var maxTilesPerJob = 100000;
+        var result = tilesResult.count + gridsResult.count;
+        if (!result) return callback(new Error('no tiles or grids'));
+
+        callback(null, Math.min(50, Math.ceil(result / maxTilesPerJob)));
+      });
     });
   });
 };
