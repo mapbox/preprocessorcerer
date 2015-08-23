6e016a17b8b092744aa5a478231fd9fb74c5e2fb
Young Hahn <young@mapbox.com>
2015-08-23 04:11:57 -0400

    Partition rasters less generously than other untiled formats.

--- parts/bySize.js
+++ parts/bySize.js
@@ -2,12 +2,15 @@ module.exports = function splitBySize(filepath, info, callback) {
   var max = 50;
   var mb = info.size / 1024 / 1024;
   var pretiled = ['serialtiles', 'tilejson', 'mbtiles'];
-  var untiled = ['zip', 'gpx', 'kml', 'geojson', 'csv', 'tif'];
+  var untiled = ['zip', 'gpx', 'kml', 'geojson', 'csv'];
+  var raster = ['tif'];
 
   if (pretiled.indexOf(info.filetype) !== -1) {
     callback(null, Math.min(max, Math.ceil(mb / 100)));
   } else if (untiled.indexOf(info.filetype) !== -1) {
     callback(null, Math.min(max, Math.ceil(mb / 10)));
+  } else if (raster.indexOf(info.filetype) !== -1) {
+    callback(null, Math.min(max, Math.ceil(mb / 100)));
   } else {
     callback(null, 1);
   }
