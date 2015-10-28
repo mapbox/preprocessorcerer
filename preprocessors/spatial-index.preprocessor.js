fc687af171c784871d5129d3a3afce821380faf1
Dane Springmeyer <dane@mapbox.com>
2015-10-28 10:09:01 -0700

    fix syntax

--- preprocessors/spatial-index.preprocessor.js
+++ preprocessors/spatial-index.preprocessor.js
@@ -6,18 +6,20 @@ var mapnik_index = path.resolve(mapnik.module_path, 'mapnik-index');
 var mkdirp = require('mkdirp');
 
 module.exports = function(infile, outfile, callback) {
+  function copy(finished) {
+    fs.createReadStream(infile)
+      .pipe(fs.createWriteStream(outfile))
+      .on('finish', finished);
+  }
+
   mkdirp(outfile, function(err) {
     if (err) return callback(err);
-      var outname = path.join(outfile,path.basename(infile));
-      function copy(create_index) {
-  fs.createReadStream(infile)
-    .pipe(fs.createWriteStream(outname))
-    .on('finish', create_index);
-       }
-       copy(function() {
-   spawn(mapnik_index,[ outname ])
-     .once('error', callback)
-     .on('exit', callback);
+    var outname = path.join(outfile, path.basename(infile));
+
+    copy(function() {
+      spawn(mapnik_index, [outname])
+        .once('error', callback)
+        .on('exit', callback);
     });
   });
 };
@@ -25,12 +27,12 @@ module.exports = function(infile, outfile, callback) {
 module.exports.description = 'Add a spatial index to GeoJSON or CSV';
 
 module.exports.criteria = function(infile, info, callback) {
-    if (info.filetype !== 'geojson' && info.filetype !== 'csv') {
-      return callback(null, false);
-    }
-    // check size is warrants creating an index
-    if (info.size > 50 * 1024 * 1024) { // 50Mb
-      return callback(null, true);
-    }
+  if (info.filetype !== 'geojson' && info.filetype !== 'csv') {
     return callback(null, false);
+  }
+  // check size is warrants creating an index
+  if (info.size > 50 * 1024 * 1024) { // 50Mb
+    return callback(null, true);
+  }
+  return callback(null, false);
 };
