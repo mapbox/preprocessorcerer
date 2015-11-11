65ba476451e6cdfdb60578c63143f8784cab9666
Dane Springmeyer <dane@mapbox.com>
2015-11-11 11:43:21 -0800

    fix syntax in test

--- test/duplicate_module.pretest.js
+++ test/duplicate_module.pretest.js
@@ -1,32 +1,32 @@
 var tape = require('tape');
 var exec = require('child_process').exec;
 
-var count_module = function (name, callback) {
-    var cmd = 'npm ls ' + name;
-    exec(cmd,
-        function (error, stdout, stderr) {
-            var pattern = new RegExp(name + '@', 'g');
-            var match = stdout.match(pattern);
-            if (!match) {
-                return callback(null, 0);
-            }
-            return callback(null, match.length);
-        });
+var count_module = function(name, callback) {
+  var cmd = 'npm ls ' + name;
+  exec(cmd, function(error, stdout) {
+    var pattern = new RegExp(name + '@', 'g');
+    var match = stdout.match(pattern);
+    if (!match) {
+      return callback(null, 0);
+    }
+
+    return callback(null, match.length);
+  });
 };
 
 [
-    'mapnik',
-    'sqlite3',
-    'gdal',
-    'tilelive',
-    'mbtiles'
+  'mapnik',
+  'sqlite3',
+  'gdal',
+  'tilelive',
+  'mbtiles'
 ].forEach(function(mod) {
-    tape.test('there should only be one ' + mod + ' module, otherwise you are asking for pwnage', function (t) {
-        count_module(mod, function (err, count) {
-            if (err) throw err;
-            t.notEqual(count, 0);
-            t.equal(count, 1);
-            t.end();
-        });
+  tape.test('there should only be one ' + mod + ' module, otherwise you are asking for pwnage', function(t) {
+    count_module(mod, function(err, count) {
+      if (err) throw err;
+      t.notEqual(count, 0);
+      t.equal(count, 1);
+      t.end();
     });
+  });
 });
