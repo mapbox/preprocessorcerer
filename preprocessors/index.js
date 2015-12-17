565ffc39ff5ea0e735c3e424e61b4bc8b36897f1
gretacb <carol@mapbox.com>
2015-12-16 21:34:26 -0500

    disable spatial index preprocessor

--- preprocessors/index.js
+++ preprocessors/index.js
@@ -9,8 +9,7 @@ var preprocessors = [
   'tif-toBytes.preprocessor',
   'tif-reproject.preprocessor',
   'shp-index.preprocessor',
-  'geojson-bom.preprocessor',
-  'spatial-index.preprocessor'
+  'geojson-bom.preprocessor'
 ];
 
 // Loads each *.preprocessor.js file and builds an array of them
